e466f7ca345dba3c8591d6821692b5baeb53f9bd
jiexdrop <jorge.noogueira@gmail.com>
2022-06-15 17:29:15 +0200

    Tried to awnser observers openclassrooms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,30 +9,47 @@ import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap } fro
 })
 export class AppComponent implements OnInit {
 
-  redTrainsCalled = 0;
-  yellowTrainsCalled = 0;
-
   ngOnInit() {
-    interval(500).pipe(
-      take(10), // Limite a 10 trains
-      map(value => value % 2 === 0 ? 'rouge' : 'jaune'), // Soit rouge soit jaune
-      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      mergeMap(color => this.getTrainObservable$(color)),
-      // tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    // const interval$ = interval(500);
+    // interval$.subscribe();
+    //interval(1000).subscribe(value => console.log(value !== 3 ? 'Tick' : 'BANG'));
+    //interval(1000).subscribe(value => console.log(value % 3 !== 0 ? 'Tick' : 'BANG'));
+
+    // interval(500).pipe(
+    //   map(value => 2 * (value + 1)),
+    //   tap(value => console.log(value))
+    // ).subscribe();
+
+    interval(1000).pipe(
+      take(3),
+      map(value => getValString(value)),
+      tap(value => console.log(value)),
+      mergeMap(message => processMessage$(message))
     ).subscribe();
+
+    function processMessage$(message: string) {
+      
+      return of(message).pipe(
+        delay(3000),
+        tap(value => console.log(value + "processed")),
+      );
+    }
+
+    function getValString(value: number): string {
+      if (value == 0) {
+        return "first";
+      }
+      else if (value == 1) {
+        return "second";
+      } else {
+        return "third";
+      }
+    }
   }
 
-  getTrainObservable$(color: 'rouge' | 'jaune') {
-    const isRedTrain = color === 'rouge';
-    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-    return of({ color, trainIndex }).pipe( // Observateur interne ?
-      delay(isRedTrain ? 5000 : 6000)
-    );
-  }
 
-  translateColor(color: 'rouge' | 'jaune') {
-    return color === 'rouge' ? 'red' : 'yellow';
-  }
 }
+
+
+
+
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject, tap } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
@@ -7,14 +9,23 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) { }
- 
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+    interval(1000).pipe(
+      tap(console.log),
+      takeUntil(this.destroy$),
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
