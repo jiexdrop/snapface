3f28a4221ca3ae650d02a6e7cbcc027f1d48fbb9
jiexdrop <jorge.noogueira@gmail.com>
2022-06-16 17:45:12 +0200

    Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,8 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snaps/components/face-snap/face-snap.component';
-
-import { FaceSnapListComponent } from './face-snaps/components/face-snap-list/face-snap-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './face-snaps/components/single-face-snap/single-face-snap.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewFaceSnapComponent } from './face-snaps/components/new-face-snap/new-face-snap.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { FaceSnapsModule } from './face-snaps/face-snaps.module';
@@ -17,7 +11,6 @@ import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
