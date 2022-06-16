10e4f297385bec14dab4722aa25d96d274d05ef5
jiexdrop <jorge.noogueira@gmail.com>
2022-06-16 09:44:36 +0200

    New reactive face snap

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewFaceSnapComponent implements OnInit {
 
-  constructor() { }
+  snapForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null],
+    });
+  }
+
+  onSubmitForm(){
+    console.log(this.snapForm.value);
   }
 
 }
